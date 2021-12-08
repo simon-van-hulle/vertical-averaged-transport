e9d1a823c74bfa715811142ab96a57cf215c0af3
Simon Van Hulle <simonvanhulle01@gmail.com>
2021-12-08 15:37:18 +0100

    Now using numpy arrays

--- src/helpers.py
+++ src/helpers.py
@@ -1,6 +1,24 @@
 """
-Very simple Logger class without fancy features. Just logging
+Some things that can be used in any file
 """
+
+import time
+
+
+def timing(func):
+    def wrapper(*args, **kwargs):
+        separator = "-"
+        separate = 79 * separator
+        print(separate)
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        print(f"\n{func.__name__} took {end_time - start_time:6.2f} seconds "
+              f"to execute")
+        print(separate)
+        return result
+    return wrapper
+
 
 CRITICAL = 50
 FATAL = CRITICAL
@@ -11,10 +29,11 @@ INFO = 20
 DEBUG = 10
 NOTSET = 0
 
+
 class Logger:
     def __init__(self, level=NOTSET):
         self.level = level
-    
+
     def debug(self, *args, **kwargs):
         if self.level <= DEBUG:
             print('[  DEBUG ]', *args, **kwargs)
@@ -22,7 +41,7 @@ class Logger:
     def info(self, *args, **kwargs):
         if self.level <= INFO:
             print('[  INFO  ]', *args, **kwargs)
-    
+
     def warn(self, *args, **kwargs):
         if self.level <= WARNING:
             print('[  WARN  ]', *args, **kwargs)
@@ -34,9 +53,9 @@ class Logger:
     def critical(self, *args, **kwargs):
         if self.level <= CRITICAL:
             print('[CRITICAL]', *args, **kwargs)
-    
+
     def warning(self, *args, **kwargs):
         self.warn(*args, **kwargs)
-    
+
     def fatal(self, *args, **kwargs):
         self.critical(*args, **kwargs)
