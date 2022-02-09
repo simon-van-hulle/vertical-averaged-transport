80a32150b4fd41f614f1b10554191a06104122d3
s.b.e.vanhulle@student.tudelft.nl <s.b.e.vanhulle@student.tudelft.nl>
2022-02-09 08:43:11 +0100

    Remove unnecessary results and plots

--- scripts/plot_leaving_domain.py
+++ scripts/plot_leaving_domain.py
@@ -9,16 +9,17 @@ import particle_model.helpers as h
 
 logger = h.easy_logger(__name__, logging.INFO)
 
-def plot_file(filename):
+def plot_file(filename, label):
 
     data= np.genfromtxt(filename).T
     dts = data[0]
     percentages = data[1]
 
-    plt.scatter(dts, percentages)
+    plt.scatter(dts, percentages, label=label)
     plt.axvline(np.max(dts), color='k', linestyle='--')
     plt.xlabel(f"Time step $\Delta t$")
     plt.ylabel(f"Percentage of particles that left the domain.")
+    plt.legend()
     plt.tight_layout()
 
 
@@ -27,8 +28,8 @@ if __name__ == "__main__":
     if len(sys.argv) < 2:
         logger.error("Expected file name")
 
-    for filename in sys.argv[1:]:
-        plot_file(filename)
+    for filename, label in zip(sys.argv[1:], ["Euler", "Milstein"]):
+        plot_file(filename, label)
     plt.axhline(0, color='k')
     plt.axvline(0, color='k')
 
